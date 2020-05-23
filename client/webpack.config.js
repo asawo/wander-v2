6ae14f66ef8bab05ac50e05b5219ef96b192dba3
asawo <asawo@mercari.com>
2020-05-23 11:44:09 +0900

    added html-webpack-plugin to solve missing index.html issue

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.tsx',
@@ -6,6 +7,7 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle.js',
 	},
+	plugins: [new HtmlWebpackPlugin()],
 	module: {
 		rules: [
 			{
--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,10 @@ app.use(express.json());
 
 app.use(express.static(path.resolve(__dirname, '../../client/dist')));
 
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+// });
+
 app.listen(PORT, () => {
     console.log(`Server is listening on localhost:${PORT}`);
 });
