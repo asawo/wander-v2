433c7defe56787ef492400a8cdfe8e9125c4fb03
asawo <asawo@mercari.com>
2020-06-08 22:02:48 +0900

    added imported dependencies

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 // import { PORT } from './config/constants';
+// import * as db from './config/database';
+import * as statusController from './controllers/status';
 import path from 'path';
 const PORT = process.env.PORT || 8080;
 
@@ -8,10 +10,8 @@ app.use(express.json());
 
 app.use(express.static(path.resolve(__dirname, '../../client/dist')));
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
-// });
+app.use('/user', require('./routes/user'));
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on localhost:${PORT}`);
+    console.log(`Server is listening on http://localhost:${PORT}`);
 });
