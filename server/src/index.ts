86b8f30a0d76bec83541711ca712921ef87f2f3c
asawo <asawo@mercari.com>
2020-06-22 16:48:33 +0900

    try #2 to connect db to the prod db

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,7 +17,7 @@ app.use('/user', userRoutes);
 
 // error middleware
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  res.status(500).json({ message: err.message });
+  res.status(500).json({ message: err.message, error: err });
 });
 
 test1(1);
